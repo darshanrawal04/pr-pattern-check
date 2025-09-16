7c67c9b0e6ba99bc69a14324663b5e24e92a990f
Darshan Rawal <drawal@nicesoftwaresolutions.com>
2025-09-16 17:19:36 +0530

    Add bad_example2 with risky patterns for workflow test

--- tests/bad_example.py
+++ tests/bad_example.py
@@ -1,2 +1,9 @@
 # hello world 
 
+df = spark.read.format("delta").option("path", "/mnt/raw/data").load()
+dt = DeltaTable.forPath(spark, "/mnt/secure/table")
+df2 = spark.read.option("path", "/mnt/test/data").load()
+
+df3 = df.withColumn("src_file", input_file_name())
+
+
