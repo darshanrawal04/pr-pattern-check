da36a5a31b0605865788074d3b47179ab6b1a8bb
Darshan Rawal <drawal@nicesoftwaresolutions.com>
2025-09-16 17:21:34 +0530

    Remove risky code from bad_example2

--- tests/bad_example.py
+++ tests/bad_example.py
@@ -1,9 +1,9 @@
 # hello world 
 
-df = spark.read.format("delta").option("path", "/mnt/raw/data").load()
-dt = DeltaTable.forPath(spark, "/mnt/secure/table")
-df2 = spark.read.option("path", "/mnt/test/data").load()
+# df = spark.read.format("delta").option("path", "/mnt/raw/data").load()
+# dt = DeltaTable.forPath(spark, "/mnt/secure/table")
+# df2 = spark.read.option("path", "/mnt/test/data").load()
 
-df3 = df.withColumn("src_file", input_file_name())
+# df3 = df.withColumn("src_file", input_file_name())
 
 
